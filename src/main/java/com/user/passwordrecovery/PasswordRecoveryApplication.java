3d867f99f41d7ee92be4f87dc0be278b1dc90414
itsYd <yemane.berhane@miu.edu>
2024-03-09 09:06:24 -0500

    PasswordRecovery to Dani

---
+++ src/main/java/com/user/passwordrecovery/PasswordRecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.user.passwordrecovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordRecoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordRecoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/user/passwordrecovery/PasswordRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.passwordrecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordRecoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
